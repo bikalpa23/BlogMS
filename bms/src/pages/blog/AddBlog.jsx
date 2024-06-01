6ed03696c2587d1e1bde17d1052debdee2f25a7e
Bikalpa Sangat <bikalpasangat1@gmail.com>
2024-06-01 22:39:10 +0545

    added pages

--- bms/src/pages/blog/AddBlog.jsx
+++ bms/src/pages/blog/AddBlog.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Navbar from '../../components/Navbar'
 
 const AddBlog = () => {
   return (
-    <div>AddBlog</div>
+    <>
+      <Navbar />
+    </>
   )
 }
 
---
+++ bms/src/pages/blog/components/card/card.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const card = () => {
+    return (
+        <>
+            <div className="max-w-sm rounded overflow-hidden shadow-lg mb-5">
+                <img className="w-full" src="https://t3.ftcdn.net/jpg/05/83/22/42/360_F_583224250_s7QIQ0baa0ZF2Ol3Iw4XRAKY6t1yZZ5x.jpg" alt="Sunset in the mountains" />
+                <div className="px-6 py-4">
+                    <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+                    <p className="text-gray-700 text-base">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                    </p>
+                </div>
+                <div className="px-6 pt-4 pb-2 mb-6">
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default card
