aa27c8795a5fed27c769c76b43157ca30d46ea54
Bikalpa Sangat <bikalpasangat1@gmail.com>
2024-06-03 16:41:19 +0545

    created components

--- bms/src/pages/blog/components/card/form/Form.jsx
+++ bms/src/pages/blog/components/card/form/Form.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Form = ({type}) => {
     return (
         <>
-            <div class="flex justify-center items-center w-screen h-screen bg-white">
+            <div class="flex justify-center items-center  h-screen bg-white">
                 <div className="container mx-auto my-4 px-4 lg:px-20">
 
                     <div className="w-full p-4 my-4 md:px-12 lg:w-9/12 lg:pl-20 lg:pr-40 mr-auto rounded-2xl shadow-2xl mx-20">
